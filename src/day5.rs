d54470ed49bc93c2290bc07c0c5a5155070c9900
Jed Davis <jld@panix.com>
2015-12-05 08:14:25 -0800

    Day 5: Fast Santa passes the tests.

--- src/day5.rs
+++ src/day5.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
 use std::hash::Hash;
+use std::rc::Rc;
+use std::marker::PhantomData;
 
-trait Scanner: Hash + Eq {
+trait Scanner {
     fn zero() -> Self;
     fn step(self, c: char) -> Self;
     fn nice(&self) -> bool;
@@ -13,6 +16,79 @@ fn nice<S: Scanner>(state: S, s: &str) -> bool {
     }
     state.nice()
 }
+
+
+#[derive(Clone, Debug)]
+struct Tabulate<S: Scanner + Hash + Eq + Clone> {
+    tab: Rc<Tables>,
+    state: Idx,
+    // So this is kind of sketchy:
+    phantom: PhantomData<S>
+}
+#[derive(PartialEq, Eq, Debug)]
+struct Tables {
+    step: Box<[[Idx; LETTERS]]>,
+    nice: Box<[bool]>,
+}
+const LETTERS: usize = 27;
+const LBASE: char = '`';
+type Idx = u16;
+impl Tables {
+    fn lidx_char(i: usize) -> char { (LBASE as u8 + i as u8) as char }
+    fn char_lidx(c: char) -> usize {
+        let i = (c as usize).wrapping_sub(LBASE as usize);
+        if i < LETTERS { i } else { 0 }
+    }
+    fn make<S: Scanner + Hash + Eq + Clone>() -> Tables {
+        let z = S::zero();
+        let mut stoi = HashMap::new();
+        let mut itos = Vec::new();
+        let mut i = 0;
+        let mut step_acc = Vec::new();
+        let mut nice_acc = Vec::new();
+        stoi.insert(z.clone(), 0);
+        itos.push(z);
+        while i < itos.len() {
+            let s = itos[i].clone();
+            let mut step = [!0; LETTERS]; // Sigh.
+            for l in 0..LETTERS {
+                let sl = s.clone().step(Tables::lidx_char(l));
+                step[l] = *(stoi.entry(sl.clone()).or_insert_with(|| {
+                    let il = itos.len() as Idx;
+                    itos.push(sl);
+                    il
+                }));
+            }
+            i += 1;
+            step_acc.push(step);
+            nice_acc.push(s.nice());
+        }
+        assert_eq!(i, step_acc.len());
+        assert_eq!(i, nice_acc.len());
+        Tables {
+            step: step_acc.into_boxed_slice(),
+            nice: nice_acc.into_boxed_slice(),
+        }
+    }
+}
+
+impl<S: Scanner + Hash + Eq + Clone> Scanner for Tabulate<S> {
+    fn zero() -> Tabulate<S> {
+        Tabulate {
+            tab: Rc::new(Tables::make::<S>()),
+            state: 0,
+            phantom: PhantomData
+        }
+    }
+    fn step(self, c: char) -> Tabulate<S> {
+        let next = self.tab.step[self.state as usize][Tables::char_lidx(c)];
+        Tabulate { state: next, ..self }
+    }
+    fn nice(&self) -> bool {
+        self.tab.nice[self.state as usize]
+    }
+}
+
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
 struct Vowels(u8);
@@ -111,10 +187,11 @@ impl<S: Scanner, T: Scanner> Scanner for Both<S, T> {
 }
 
 type Santa = Both<Vowels, Both<Doubled, Censor>>;
+type FastSanta = Tabulate<Santa>;
 
 #[cfg(test)]
 mod test {
-    use super::{Scanner, Vowels, Doubled, Censor, Santa, nice};
+    use super::{Scanner, Vowels, Doubled, Censor, Santa, FastSanta, nice};
 
     #[test]
     fn spec_line1() {
@@ -159,6 +236,17 @@ mod test {
         let word = "dvszwmarrgswjxmb";
         assert!(!nice(Vowels::zero(), word));
         assert!(!nice(Santa::zero(), word));
+    }
+
+    #[test]
+    fn fast_santa() {
+        let z = FastSanta::zero();
+        assert!(nice(z.clone(), "ugknbfddgicrmopn"));
+        assert!(nice(z.clone(), "aaa"));
+        assert!(!nice(z.clone(), "jchzalrnumimnmhp"));
+        assert!(!nice(z.clone(), "haegwjzuvuyypxyu"));
+        assert!(!nice(z.clone(), "haegwjzuvuypxxyu"));
+        assert!(!nice(z.clone(), "dvszwmarrgswjxmb"));
     }
 }
 
