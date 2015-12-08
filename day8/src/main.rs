0751ea2eedcde1bc4fd993bbb6aaef03bb74fae8
Jed Davis <jld@panix.com>
2015-12-08 08:37:34 -0500

    Day 8: part 2.  Nothing too exciting here.

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -16,32 +16,30 @@ fn is_hexdigit(c: char) -> bool {
     }
 }
 
-fn compute<I: Iterator<Item=char>>(chars: I) -> u64 {
+fn compute<I: Iterator<Item=char>>(chars: I) -> (u64, u64) {
     let mut waste = 0;
+    let mut expand = 0;
     let mut state = State::Unquote;
     for char in chars {
-        let wasted = match (state, char) {
-            (State::Unquote, '"') => { state = State::Quote; true },
-            (State::Unquote, _) => false,
-            (State::Quote, '"') => { state = State::Unquote; true },
-            (State::Quote, '\\') => { state = State::Backslash; true },
-            (State::Quote, _) => false,
-            (State::Backslash, '\\') |
-            (State::Backslash, 'n') |
-            (State::Backslash, '"') => { state = State::Quote; false },
-            (State::Backslash, 'x') => { state = State::Hex0; true },
+        match (state, char) {
+            (State::Unquote, '"') => { state = State::Quote; waste += 1; expand += 2 },
+            (State::Unquote, _) => (),
+            (State::Quote, '"') => { state = State::Unquote; waste += 1; expand += 2 },
+            (State::Quote, '\\') => { state = State::Backslash; waste += 1; expand += 1 },
+            (State::Quote, _) => (),
+            (State::Backslash, '"') |
+            (State::Backslash, '\\') => { state = State::Quote; expand += 1 },
+            (State::Backslash, 'n') => { state = State::Quote; },
+            (State::Backslash, 'x') => { state = State::Hex0; waste += 1 },
             (State::Backslash, _) => panic!("bad escape char {:?}", char),
-            (State::Hex0, c) if is_hexdigit(c) => { state = State::Hex1; true },
-            (State::Hex1, c) if is_hexdigit(c) => { state = State::Quote; false },
+            (State::Hex0, c) if is_hexdigit(c) => { state = State::Hex1; waste += 1 },
+            (State::Hex1, c) if is_hexdigit(c) => { state = State::Quote;  },
             (State::Hex0, _) |
             (State::Hex1, _) => panic!("bad hexdigit {:?}", char),
-        };
-        if wasted {
-            waste += 1;
         }
     }
     assert!(state == State::Unquote, "unterminated syntax (EOF in {:?})", state);
-    waste
+    (waste, expand)
 }
 
 pub fn main() {
@@ -49,30 +47,53 @@ pub fn main() {
     // Sadly, io::Chars is unstable.
     let mut buf = String::new();
     stdin.lock().read_to_string(&mut buf).unwrap();
-    let waste = compute(buf.chars());
+    let (waste, expand) = compute(buf.chars());
     println!("Wasted {} char{}.", waste, if waste == 1 { "" } else { "s" });
+    println!("Expanded by {} char{}.", expand, if expand == 1 { "" } else { "s" });
 }
 
 #[cfg(test)]
 mod test {
     use super::compute;
 
+    fn waste(s: &str) -> u64 { let (w, _e) = compute(s.chars()); w }
+    fn expn(s: &str) -> u64 { let (_w, e) = compute(s.chars()); e }
+
     #[test]
     fn unquoted() {
-        assert_eq!(compute("chicken".chars()), 0);
-        assert_eq!(compute("chicke\\n".chars()), 0);
+        assert_eq!(waste("chicken"), 0);
+        assert_eq!(waste("chicke\\n"), 0);
     }
 
     #[test]
     fn examples_severally() {
-        assert_eq!(compute("\"\"".chars()), 2 - 0);
-        assert_eq!(compute("\"abc\"".chars()), 5 - 3);
-        assert_eq!(compute("\"aaa\\\"aaa\"".chars()), 10 - 7);
-        assert_eq!(compute("\"\\x27\"".chars()), 6 - 1);
+        assert_eq!(waste("\"\""), 2 - 0);
+        assert_eq!(waste("\"abc\""), 5 - 3);
+        assert_eq!(waste("\"aaa\\\"aaa\""), 10 - 7);
+        assert_eq!(waste("\"\\x27\""), 6 - 1);
     }
-    
+
     #[test]
     fn examples_jointly() {
-        assert_eq!(compute("\"\"\n\"abc\"\n\"aaa\\\"aaa\"\n\"\\x27\"\n".chars()), 23 - 11);
+        assert_eq!(waste("\"\"\n\"abc\"\n\"aaa\\\"aaa\"\n\"\\x27\"\n"), 23 - 11);
+    }
+
+    #[test]
+    fn unquoted2() {
+        assert_eq!(expn("chicken"), 0);
+        assert_eq!(expn("chicke\\n"), 0);
+    }
+
+    #[test]
+    fn examples2_severally() {
+        assert_eq!(expn("\"\""), 6 - 2);
+        assert_eq!(expn("\"abc\""), 9 - 5);
+        assert_eq!(expn("\"aaa\\\"aaa\""), 16 - 10);
+        assert_eq!(expn("\"\\x27\""), 11 - 6);
+    }
+
+    #[test]
+    fn examples2_jointly() {
+        assert_eq!(expn("\"\"\n\"abc\"\n\"aaa\\\"aaa\"\n\"\\x27\"\n"), 42 - 23);
     }
 }
