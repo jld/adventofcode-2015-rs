baafd18c9da1f3c77258bdea336a3f1cb2beba3f
Jed Davis <jld@panix.com>
2015-12-13 15:35:33 -0800

    Factor out the set/stack thing from day9.

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -3,7 +3,6 @@ extern crate util;
 mod reader;
 
 use std::io::{stdin, BufRead};
-use std::mem::size_of;
 
 type Dist = usize;
 type Grid = reader::Grid<Dist>;
@@ -16,38 +15,7 @@ impl Cmp for Shortest { fn better(&self, old: Dist, shiny: Dist) -> bool { shiny
 struct Longest;
 impl Cmp for Longest { fn better(&self, old: Dist, shiny: Dist) -> bool { shiny > old } }
 
-struct State {
-    stack: Vec<u8>,
-    mask: Mask,
-}
-impl State {
-    fn new(n: usize) -> State {
-        assert!(n < size_of::<Mask>() * 8, "{} points is too many", n);
-        State { stack: Vec::new(), mask: !0 << n }
-    }
-    fn push<'a, F>(&mut self, i: usize, f: F) where F: FnOnce(&mut State) {
-        if !self.contains(i) {
-            self.stack.push(i as u8);
-            self.mask |= 1 << i;
-            f(self);
-            let _i = self.stack.pop().unwrap();
-            debug_assert_eq!(_i as usize, i);
-            self.mask &= !(1 << i);
-        }
-    }
-    fn top(&self) -> usize {
-        self.stack[self.stack.len() - 1] as usize
-    }
-    fn len(&self) -> usize {
-        self.stack.len()
-    }
-    fn contains(&self, i: usize) -> bool {
-        self.mask & 1 << i != 0
-    }
-    fn is_full(&self) -> bool {
-        self.mask == !0
-    }
-}
+type State = util::StackSet<u32>;
 
 struct Best<C: Cmp> {
     path: Vec<u8>,
@@ -70,15 +38,15 @@ impl<C: Cmp> Best<C> {
 
 fn search<C: Cmp>(g: &Grid, st: &mut State, be: &mut Best<C>, so_far: Dist) {
     if st.is_full() {
-        be.add(so_far, &st.stack);
+        be.add(so_far, st);
         return;
     }
     // For C==Shortest this could prune if so_far >= be.dist.
     // For C==Longest, would need an estimate of largest possible rest-of-path.
-    let i = st.top();
+    let i = *st.last().unwrap() as usize;
     for (j, od) in g[i].iter().enumerate() {
         if let Some(d) = *od {
-            st.push(j, |st| search(g, st, be, so_far + d));
+            st.push(j as u8, |st| search(g, st, be, so_far + d));
         }
     }
 }
@@ -91,12 +59,10 @@ fn parse<B: BufRead>(input: B) -> Problem {
 
 fn compute<C: Cmp>(p: &Problem, cmp: C) -> (Dist, Vec<String>) {
     let &(ref stab, ref g) = p;
-    let mut st = State::new(g.len());
+    let mut st = State::new(g.len()).expect("too many points");
     let mut be = Best::new(cmp);
     for i in 0..g.len() {
-        debug_assert!(st.len() == 0);
-        debug_assert!(!st.contains(i));
-        st.push(i, |st| search(&g, st, &mut be, 0));
+        st.push(i as u8, |st| search(&g, st, &mut be, 0));
     }
     (be.dist.expect("No path!?"), be.path.iter().map(|i| stab.print(*i as usize)).collect())
 }
--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod autovec;
 pub mod symtab;
 pub mod write_once;
+pub mod stack_set;
 
 pub use autovec::AutoVec;
 pub use symtab::SymTab;
 pub use write_once::WriteOnce;
+pub use stack_set::StackSet;
