97003b65a55d8437ca4e1fb9b9d680133bea659d
Jed Davis <jld@panix.com>
2015-12-13 18:05:37 -0800

    Move yet more of day9 into a reusable library.

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,4 +1,5 @@
 extern crate util;
+use util::best::{Smallest,Largest,Cmp};
 
 mod reader;
 
@@ -7,42 +8,16 @@ use std::io::{stdin, BufRead};
 type Dist = usize;
 type Grid = reader::Grid<Dist>;
 type Problem = (reader::SymTab, Grid);
-type Mask = usize;
-
-trait Cmp { fn better(&self, old: Dist, shiny: Dist) -> bool; }
-struct Shortest;
-impl Cmp for Shortest { fn better(&self, old: Dist, shiny: Dist) -> bool { shiny < old } }
-struct Longest;
-impl Cmp for Longest { fn better(&self, old: Dist, shiny: Dist) -> bool { shiny > old } }
-
 type State = util::StackSet<u32>;
+type Best<C> = util::Best<Dist, Vec<u8>, C>;
 
-struct Best<C: Cmp> {
-    path: Vec<u8>,
-    dist: Option<Dist>,
-    cmp: C,
-}
-impl<C: Cmp> Best<C> {
-    fn new(cmp: C) -> Best<C> { Best {
-        path: Vec::new(),
-        dist: None,
-        cmp: cmp,
-    }}
-    fn add(&mut self, dist: Dist, path: &[u8]) {
-        if self.dist.as_ref().map_or(true, |&old| self.cmp.better(old, dist)) {
-            self.dist = Some(dist);
-            self.path = path.to_owned();
-        }
-    }
-}
-
-fn search<C: Cmp>(g: &Grid, st: &mut State, be: &mut Best<C>, so_far: Dist) {
+fn search<C: Cmp<Dist>>(g: &Grid, st: &mut State, be: &mut Best<C>, so_far: Dist) {
     if st.is_full() {
-        be.add(so_far, st);
+        be.add(so_far, st as &[u8]);
         return;
     }
-    // For C==Shortest this could prune if so_far >= be.dist.
-    // For C==Longest, would need an estimate of largest possible rest-of-path.
+    // For C==Smallest this could prune if so_far >= be.dist.
+    // For C==Largest, would need an estimate of largest possible rest-of-path.
     let i = *st.last().unwrap() as usize;
     for (j, od) in g[i].iter().enumerate() {
         if let Some(d) = *od {
@@ -57,28 +32,30 @@ fn parse<B: BufRead>(input: B) -> Problem {
     (stab, g)
 }
 
-fn compute<C: Cmp>(p: &Problem, cmp: C) -> (Dist, Vec<String>) {
+fn compute<C: Cmp<Dist>>(p: &Problem, cmp: C) -> (Dist, Vec<String>) {
     let &(ref stab, ref g) = p;
     let mut st = State::new(g.len()).expect("too many points");
     let mut be = Best::new(cmp);
     for i in 0..g.len() {
         st.push(i as u8, |st| search(&g, st, &mut be, 0));
     }
-    (be.dist.expect("No path!?"), be.path.iter().map(|i| stab.print(*i as usize)).collect())
+    let (dist, path) = be.expect("No path!?");
+    (dist, path.iter().map(|i| stab.print(*i as usize)).collect())
 }
 
 pub fn main() {
     let stdin = stdin();
     let prob = parse(stdin.lock());
-    let (sdist, splaces) = compute(&prob, Shortest);
-    println!("Shortest: {}; {}", sdist, splaces.join(" -> "));
-    let (ldist, lplaces) = compute(&prob, Longest);
-    println!("Longest: {}; {}", ldist, lplaces.join(" -> "));
+    let (sdist, splaces) = compute(&prob, Smallest);
+    println!("Smallest: {}; {}", sdist, splaces.join(" -> "));
+    let (ldist, lplaces) = compute(&prob, Largest);
+    println!("Largest: {}; {}", ldist, lplaces.join(" -> "));
 }
 
 #[cfg(test)]
 mod test {
-    use super::{compute,parse,Shortest,Longest};
+    use util::best::{Smallest,Largest};
+    use super::{compute,parse};
 
     const EXAMPLE: &'static str = "\
         London to Dublin = 464\n\
@@ -87,7 +64,7 @@ mod test {
 
     #[test]
     fn example() {
-        let (dist, path) = compute(&parse(EXAMPLE.as_bytes()), Shortest);
+        let (dist, path) = compute(&parse(EXAMPLE.as_bytes()), Smallest);
         assert_eq!(dist, 605);
         assert_eq!(path.len(), 3);
         assert_eq!(path[0], "London");
@@ -97,7 +74,7 @@ mod test {
 
     #[test]
     fn example_long() {
-        let (dist, path) = compute(&parse(EXAMPLE.as_bytes()), Longest);
+        let (dist, path) = compute(&parse(EXAMPLE.as_bytes()), Largest);
         assert_eq!(dist, 982);
         assert_eq!(path.len(), 3);
         assert_eq!(path[0], "Dublin");
--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod autovec;
+pub mod best;
 pub mod symtab;
 pub mod write_once;
 pub mod stack_set;
 
 pub use autovec::AutoVec;
+pub use best::Best;
 pub use symtab::SymTab;
 pub use write_once::WriteOnce;
 pub use stack_set::StackSet;
