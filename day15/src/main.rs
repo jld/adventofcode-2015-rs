5873e3fb8eb007e82e5d51ef34d0a7ab3986b12d
Jed Davis <jld@panix.com>
2015-12-16 09:07:16 -0800

    Day 15: part 1 works.

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -4,7 +4,11 @@ mod search;
 
 use std::cmp::max;
 use std::convert::From;
+use std::io::{stdin,BufRead};
 use std::ops::{Add, Mul};
+
+use parser::{parse,Error};
+use search::exhaustive;
 
 pub type Num = i64;
 pub type Qty = u8;
@@ -55,7 +59,28 @@ impl<N: Copy> Mul<N> for Stats where Num: From<N> {
     }
 }
 
-
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let mut names = Vec::new();
+    let mut ingrs = Vec::new();
+    for (ln, line) in stdin.lock().lines().enumerate() {
+        let line = line.expect("I/O error");
+        match parse(&line) {
+            Err(Error::NumberFail) => panic!("line {}: bad number", ln),
+            Err(Error::SpaceFail) => panic!("line {}: attribute must be name' 'value pair", ln),
+            Err(Error::ColonFail) => panic!("line {}: ingredient must be name':'stats pair", ln),
+            Err(Error::MissingStat(name)) => panic!("line {}: missing attribute {}", ln, name),
+            Err(Error::DupStat(name, v0, v1)) => panic!("line {}: attribute {} given twice, \
+                                                         as {} and {}", ln, name, v0, v1),
+            Ok((name, ingr)) => {
+                names.push(name.to_owned());
+                ingrs.push(ingr);
+            }
+        }
+    }
+    let (score, soln) = search::exhaustive(&ingrs, 100);
+    println!("Maximal cookie score: {}", score);
+    for (i, name) in names.iter().enumerate() {
+        println!("* {} tsp of {}", soln[i], name);
+    }
 }
