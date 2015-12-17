41c877319d580fb31e80de2517fdb43a4aa15731
Jed Davis <jld@panix.com>
2015-12-16 22:40:59 -0800

    Day 17: A simple approach that passes the example.

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,10 +1,31 @@
 use std::io::{stdin,BufRead};
 
+type Vol = u32;
 type Num = u64;
+
+fn exhaustive(ns: &[Vol], target: Vol) -> Num {
+    if ns.is_empty() {
+        if target == 0 { 1 } else { 0 }
+    } else {
+        exhaustive(&ns[1..], target) +
+            target.checked_sub(ns[0]).map_or(0, |nt| exhaustive(&ns[1..], nt))
+    }
+}
 
 fn main() {
     let stdin = stdin();
-    let nums: Vec<Num> =
+    let nums: Vec<Vol> =
         stdin.lock().lines().map(|l| l.expect("I/O error").parse().expect("NaN")).collect();
     println!("Hello, world! {:?}", nums);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::exhaustive;
+
+    #[test]
+    fn exh_example() {
+        assert_eq!(exhaustive(&[20, 15, 10, 5, 5], 25), 4);
+    }
 }
