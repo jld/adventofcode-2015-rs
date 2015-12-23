5e52e20a40fcf16d040236d301f822342eff56b8
Jed Davis <jld@panix.com>
2015-12-23 00:17:27 -0800

    Day 23: Part 2 was kind of obvious from desk-checking the input.
    I'd figured the twist would probably be more subtle than that, but no.
    (After day 19 I'm not exactly complaining about an easy part 2, mind.)

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -2,6 +2,20 @@ extern crate regex;
 mod parser;
 mod interp;
 
+use std::convert::Into;
+use std::io::{stdin,BufRead};
+use parser::Parser;
+use interp::Reg;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let p = Parser::new();
+    let prog: Vec<_> =
+        stdin.lock().lines().map(|rl| p.parse_line(&rl.expect("I/O error"))).collect();
+    let output0 = interp::run(&prog, [].into());
+    println!("[0] A = {}", output0[Reg::A]);
+    println!("[0] B = {}", output0[Reg::B]);
+    let output1 = interp::run(&prog, [(Reg::A, 1)].into());
+    println!("[1] A = {}", output1[Reg::A]);
+    println!("[1] B = {}", output1[Reg::B]);
 }
