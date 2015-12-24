2d2c4f68d7e18b68a9abb43fa16d46126bf43470
Jed Davis <jld@panix.com>
2015-12-24 01:00:46 -0800

    Day 24: Part 2.  But now this is slower for part 1.
    It takes 8s when it used to take 6.2s, but I didn't think the part I was
    editing was on the hot path.

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,36 +1,34 @@
 extern crate util;
 use std::cell::Cell;
 use std::cmp::Ord;
+use std::env;
 use std::io::{stdin,BufRead};
 use util::SubsetSumIter;
 
 type Weight = u16;
-type Count = u16;
 type Entangle = u64;
 type Bag = Vec<Weight>;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Plan {
-    bags: [Bag; 3],
-    ent: Entangle
+    front: Bag,
+    back: Vec<Bag>,
+    ent: Entangle,
 }
 
 impl Plan {
-    fn new(front: Bag, left: Bag, right: Bag) -> Self {
+    fn new(front: Bag, back: Vec<Bag>) -> Self {
         let ent = front.iter().fold(1 as Entangle, |e, &w| e.checked_mul(w as Entangle)
-                                    .expect("quantum entanglement overflow"));
+                                      .expect("quantum entanglement overflow"));
         Plan {
-            bags: [front, left, right],
-            ent: ent
+            front: front,
+            back: back,
+            ent: ent,
         }
     }
 
-    fn front(&self) -> &Bag { &self.bags[0] }
-    fn  left(&self) -> &Bag { &self.bags[1] }
-    fn right(&self) -> &Bag { &self.bags[2] }
-
     fn badness(&self) -> (usize, Entangle) {
-        (self.front().len(), self.ent)
+        (self.front.len(), self.ent)
     }
     fn is_better(&self, other: &Self) -> bool {
         self.badness() < other.badness()
@@ -39,23 +37,39 @@ impl Plan {
 
 #[derive(Debug, PartialEq, Eq)]
 enum Fail {
-    NonDiv3,
+    NonDiv,
     Unsat
 }
 
-fn compute(mut stuff: Vec<Weight>) -> Result<Plan, Fail> {
+// All that matters for the non-front groups is that there exist *some* equal split.
+fn whatever(stuff: Bag, groups: usize, each: Weight) -> Option<Vec<Bag>> {
+    if groups == 0 {
+        return Some(vec![]);
+    }
+    let mut split = SubsetSumIter::new(&stuff, each);
+    split.next().and_then(|(mine, theirs)| {
+        whatever(theirs, groups - 1, each).map(|mut rest| {
+            rest.push(mine);
+            rest
+        })
+    })
+}
+
+fn compute(mut stuff: Bag, groups: usize) -> Result<Plan, Fail> {
     let total = stuff.iter().fold(0 as Weight, |a, &i| a.checked_add(i).expect("weight overflow"));
-    if total % 3 != 0 { return Err(Fail::NonDiv3) }
+    if total % groups as Weight != 0 { return Err(Fail::NonDiv) }
+    let each = total / groups as Weight;
+
     stuff.sort_by(|a, b| b.cmp(a)); // Bias towards the larger elements in front.
-    let thirds = SubsetSumIter::new(&stuff, total / 3);
+    let thirds = SubsetSumIter::new(&stuff, each);
+
     let fewest = Cell::new(stuff.len() + 1);
     let front_candidates = thirds.filter(|&(ref front, ref _back)| {
         front.len() <= fewest.get() && { fewest.set(front.len()); true }
     });
+
     let balanced = front_candidates.filter_map(|(front, back)| {
-        let mut leftright = SubsetSumIter::new(&back, total / 3);
-        // All that matters is that there exist *some* equal split.
-        leftright.next().map(|(left, right)| Plan::new(front, left, right))
+        whatever(back, groups - 1, each).map(|back| Plan::new(front, back))
     });
     balanced.fold(Err(Fail::Unsat), |acc, plan| {
         match acc {
@@ -66,16 +80,26 @@ fn compute(mut stuff: Vec<Weight>) -> Result<Plan, Fail> {
 }
 
 fn main() {
+    let n: usize = env::args().nth(1).unwrap_or_else(|| "3".to_owned()).parse().expect("NaN");
+    assert!(n > 0);
+
     let stdin = stdin();
     let stuff: Bag =
         stdin.lock().lines().map(|l| l.expect("I/O error").parse().expect("NaN")).collect();
-    match compute(stuff) {
-        Err(Fail::NonDiv3) => println!("Total weight not divisible by 3!"),
+
+    match compute(stuff, n) {
+        Err(Fail::NonDiv) => println!("Total weight not divisible by {}!", n),
         Err(Fail::Unsat) => println!("Packages can't be divided evenly!"),
         Ok(plan) => {
-            println!("Front: {:?}", plan.front());
-            println!(" Left: {:?}", plan. left());
-            println!("Right: {:?}", plan.right());
+            println!("Front: {:?}", plan.front);
+            for (i, name) in [" Left", "Right", "Trunk"].iter().enumerate() {
+                if let Some(thing) = plan.back.get(i) {
+                    println!("{}: {:?}", name, thing);
+                }
+            }
+            for (i, thing) in plan.back.iter().skip(3).enumerate() {
+                println!("Other Place #{}: {:?}", i+1, thing);
+            }
             println!("-- ");
             println!("Quantum Entanglement: {}", plan.ent);
         }
@@ -88,30 +112,32 @@ mod tests {
 
     #[test]
     fn fails() {
-        assert_eq!(compute(vec![1, 1, 3]), Err(Fail::NonDiv3));
-        assert_eq!(compute(vec![1, 1, 4]), Err(Fail::Unsat));
-        assert_eq!(compute(vec![1, 2, 3]), Err(Fail::Unsat));
-        assert_eq!(compute(vec![1, 2, 3]), Err(Fail::Unsat));
-        assert_eq!(compute(vec![2, 2, 2, 3]), Err(Fail::Unsat));
+        assert_eq!(compute(vec![1, 1, 3], 3), Err(Fail::NonDiv));
+        assert_eq!(compute(vec![1, 1, 4], 3), Err(Fail::Unsat));
+        assert_eq!(compute(vec![1, 2, 3], 3), Err(Fail::Unsat));
+        assert_eq!(compute(vec![1, 2, 3], 3), Err(Fail::Unsat));
+        assert_eq!(compute(vec![2, 2, 2, 3], 3), Err(Fail::Unsat));
     }
 
     #[test]
     fn simple() {
-        assert_eq!(compute(vec![3, 3, 3]), Ok(Plan {
-            bags: [vec![3], vec![3], vec![3]],
+        assert_eq!(compute(vec![3, 3, 3], 3), Ok(Plan {
+            front: vec![3],
+            back: vec![vec![3], vec![3]],
             ent: 3
         }));
-        assert_eq!(compute(vec![2, 3, 2, 3, 2, 3]), Ok(Plan {
-            bags: [vec![3, 2], vec![3, 2], vec![3, 2]],
+        assert_eq!(compute(vec![2, 3, 2, 3, 2, 3], 3), Ok(Plan {
+            front: vec![3, 2],
+            back: vec![vec![3, 2], vec![3, 2]],
             ent: 6
         }));
     }
 
     #[test]
     fn example() {
-        let plan = compute(vec![1, 2, 3, 4, 5, 7, 8, 9, 10, 11]).unwrap();
+        let plan = compute(vec![1, 2, 3, 4, 5, 7, 8, 9, 10, 11], 3).unwrap();
         assert_eq!(plan.ent, 99);
-        assert_eq!(plan.front(), &[11, 9]);
+        assert_eq!(plan.front, &[11, 9]);
         // The problem-setter's combination enumerator seems to go
         // left-to-right and with-then-without; mine is right-to-left
         // and without-then-with, so it doesn't match.
